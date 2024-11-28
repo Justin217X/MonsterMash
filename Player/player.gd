f6558aba0ba43ce5390525c3a4e51bb39e0041a2
Justin217X <huedjustin@gmail.com>
2024-11-28 12:13:10 -0500

    Created and implemented an IceSpear Attack
    - Created an ice spear attack
    - Implemented ice spear attack
    - Added sprite and audio nodes for future implementation
    - Player now attacks using ice spear as basic attack.
    
    Note: In the future we should consider separating ice spear from player in order to have multiple characters with different basic attacks, but it does not seem likely that other characters will be added for the time being.

--- Player/player.gd
+++ Player/player.gd
@@ -4,6 +4,26 @@ extends CharacterBody2D
 var movement_speed = 400.0
 var hp = 80
 
+# Attacks
+var iceSpear = preload("res://Player/Attack/ice_spear.tscn")
+
+# AttackNodes
+@onready var iceSpearTimer = get_node("Attack/IceSpearTimer")
+@onready var iceSpearAttackTimer = get_node("Attack/IceSpearTimer/IceSpearAttackTimer")
+
+# Ice Spear
+var icespear_ammo = 0
+var icespear_baseammo = 1
+var icespear_attackspeed = 1.5
+var icespear_level = 1
+
+#Enemy Related
+var enemy_close = []
+
+func _ready():
+	attack()
+
+
 func _physics_process(delta):
 	movement()
 
@@ -14,7 +34,47 @@ func movement():
 	velocity = mov.normalized()*movement_speed
 	move_and_slide()
 
+func attack():
+	if icespear_level > 0:
+		iceSpearTimer.wait_time = icespear_attackspeed
+		if iceSpearTimer.is_stopped():
+			iceSpearTimer.start()
 
 func _on_hurt_box_hurt(damage):
 	hp -= damage
 	print(hp)
+
+
+func _on_ice_spear_timer_timeout():
+	icespear_ammo += icespear_baseammo
+	iceSpearAttackTimer.start()
+
+
+func _on_ice_spear_attack_timer_timeout():
+	if icespear_ammo > 0:
+		var icespear_attack = iceSpear.instantiate()
+		icespear_attack.position = position
+		icespear_attack.target = get_random_target()
+		icespear_attack.level = icespear_level
+		add_child(icespear_attack)
+		icespear_ammo -= 1
+		if icespear_ammo > 0:
+			iceSpearAttackTimer.start()
+		else:
+			iceSpearAttackTimer.stop()
+
+func get_random_target(): # This should be temporary. We should target closest.
+	if enemy_close.size() > 0:
+		return enemy_close.pick_random().global_position
+	else:
+		return Vector2.UP
+
+
+func _on_enemy_detection_area_body_entered(body: Node2D) -> void:
+	if not enemy_close.has(body):
+		enemy_close.append(body)
+
+
+func _on_enemy_detection_area_body_exited(body: Node2D) -> void:
+	if enemy_close.has(body):
+		enemy_close.erase(body)
