d64a2a0bf35f660491d11faea463d1f48a0600e8
Justin217X <huedjustin@gmail.com>
2024-12-10 18:50:10 -0500

    Created New Enemy Types + Fixes
    --Features--
    - Created new enemy types
    -- skeleton_weak
    -- skeleton_strong
    -- gorgon
    -- werewolf
    -- werewolf_boss
    - Updated Enemy spawner in world scene to introduce waves for 5 minutes
    -
    
    --Fixes--
    - Adjusted Tornado sound since it was too loud
    - Moved experience gem sound to proper place

--- Enemy/enemy.gd
+++ Enemy/enemy.gd
@@ -4,19 +4,21 @@ extends CharacterBody2D
 @export var movement_speed = 20.0
 @export var hp = 10
 @export var experience = 1
+@export var damage = 1
 @export var knockback_recovery = 3.5
 
 var knockback = Vector2.ZERO
 
 @onready var player = get_tree().get_first_node_in_group("player")
 @onready var loot_base = get_tree().get_first_node_in_group("loot")
+@onready var hitBox = $HitBox
 
 var exp_gem = preload("res://Objects/experience_gem.tscn")
 
 signal remove_from_array(object)
 
 func _ready() -> void:
-	pass
+	hitBox.damage = damage
 
 func _physics_process(_delta):
 	knockback = knockback.move_toward(Vector2.ZERO, knockback_recovery)
